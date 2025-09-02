f7839936795cb30055d18c8c182b1082a0cfb7ad
miscellaneous-mice <prateeknarasimha@gmail.com>
2025-09-02 21:33:56 +0100

    Improved the code

--- 01_hello/hello.c
+++ 01_hello/hello.c
@@ -1,12 +1,12 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-int my_init(void) {
+static int __init my_init(void) {
 	printk("hello - Hello, Kernel!\n");
 	return 0;
 }
 
-void my_exit(void) {
+static void __exit my_exit(void) {
 	printk("hello - Goodbye, Kernel!\n");
 }
 
@@ -14,3 +14,5 @@ module_init(my_init);
 module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("PRATEEK");
+MODULE_DESCRIPTION("Simple hello world driver");
